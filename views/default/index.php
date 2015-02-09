9edb51fb0b04b2343a40147d555e2f1e33f80463
notgosu <not.gosu@gmail.com>
2015-02-09 21:54:58 +0200

    update index view

--- views/default/index.php
+++ views/default/index.php
@@ -20,47 +20,52 @@ echo Breadcrumbs::widget(['links' => [
     $this->title
 ]]);
 ?>
-<div class="message-index">
-    <h3><?= Html::encode($this->title) ?></h3>
-    <?php
-    Pjax::begin();
-    echo GridView::widget([
-        'filterModel' => $searchModel,
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            [
-                'attribute' => 'id',
-                'value' => function ($model, $index, $dataColumn) {
-                        return $model->id;
-                    },
-                'filter' => false
-            ],
-            [
-                'attribute' => 'message',
-                'format' => 'raw',
-                'value' => function ($model, $index, $widget) {
-                        return Html::a($model->message, ['update', 'id' => $model->id], ['data' => ['pjax' => 0]]);
-                    }
-            ],
-            [
-                'attribute' => 'category',
-                'value' => function ($model, $index, $dataColumn) {
-                        return $model->category;
-                    },
-                'filter' => ArrayHelper::map($searchModel::getCategories(), 'category', 'category')
-            ],
-            [
-                'attribute' => 'status',
-                'value' => function ($model, $index, $widget) {
-                        return '';
-                    },
-                'filter' => Html::dropDownList($searchModel->formName() . '[status]', $searchModel->status, $searchModel->getStatus(), [
-                        'class' => 'form-control',
-                        'prompt' => ''
-                    ])
-            ]
-        ]
-    ]);
-    Pjax::end();
-    ?>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
+    </div>
+
+    <div class="panel-body">
+        <?php
+        Pjax::begin();
+        echo GridView::widget([
+                'filterModel' => $searchModel,
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'attribute' => 'id',
+                        'value' => function ($model, $index, $dataColumn) {
+                                return $model->id;
+                            },
+                        'filter' => false
+                    ],
+                    [
+                        'attribute' => 'message',
+                        'format' => 'raw',
+                        'value' => function ($model, $index, $widget) {
+                                return Html::a($model->message, ['update', 'id' => $model->id], ['data' => ['pjax' => 0]]);
+                            }
+                    ],
+                    [
+                        'attribute' => 'category',
+                        'value' => function ($model, $index, $dataColumn) {
+                                return $model->category;
+                            },
+                        'filter' => ArrayHelper::map($searchModel::getCategories(), 'category', 'category')
+                    ],
+                    [
+                        'attribute' => 'status',
+                        'value' => function ($model, $index, $widget) {
+                                return '';
+                            },
+                        'filter' => Html::dropDownList($searchModel->formName() . '[status]', $searchModel->status, $searchModel->getStatus(), [
+                                    'class' => 'form-control',
+                                    'prompt' => ''
+                                ])
+                    ]
+                ]
+            ]);
+        Pjax::end();
+        ?>
+    </div>
 </div>
