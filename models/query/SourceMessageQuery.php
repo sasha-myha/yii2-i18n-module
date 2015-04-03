45da92658f5b4b5f7b0d78417aefbf18af3782ce
notgosu <not.gosu@gmail.com>
2015-04-03 12:45:00 +0300

    fix scope for translated models

--- models/query/SourceMessageQuery.php
+++ models/query/SourceMessageQuery.php
@@ -33,7 +33,7 @@ class SourceMessageQuery extends ActiveQuery
         $i = 0;
         foreach (Yii::$app->getI18n()->languages as $language) {
             if ($i === 0) {
-                $query->andWhere($messageTableName . '.language = :language and ' . $messageTableName . '.translation is not null', [':language' => $language]);
+                $query->andWhere($messageTableName . '.language = :language and ' . $messageTableName . '.translation is not null and '.$messageTableName. '.translation <> ""', [':language' => $language]);
             } else {
                 $query->innerJoin($messageTableName . ' t' . $i, 't' . $i . '.id = ' . $messageTableName . '.id and t' . $i . '.language = :language and t' . $i . '.translation is not null', [':language' => $language]);
             }
